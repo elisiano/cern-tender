6f04a82e2f7adffe011c9fc9c9871e870f568f87
Elisiano Petrini <Elisiano.Petrini@cern.ch>
2012-05-02 20:55:39 +0200

    fixed questionnaire System/Section display + given a bit of look and feel (still to be improved)

--- questionnaire/views.py
+++ questionnaire/views.py
@@ -17,13 +17,18 @@ db = couchdbkit.ext.django.loading.get_db('documents')
 def questionnaire(request, doc_id):
     doc = db.get(doc_id)
     pattern = re.compile(r"sys(?P<sys>\d+)_sec(?P<sec>\d+)_q(?P<q>\d+)")
+
+    # populated while saving the doc and injected in the form fields
+    form_extra_data = {}
     if request.POST:
         form_data = doc.copy()
         form_data.update(request.POST)
 
         for f in form_data:
-            if pattern.match(str(f)):
+            match = pattern.match(str(f))
+            if match:
                 form_data[f] = form_data[f][0]
+
         form = forms.QSystemForm(form_data, auto_id=True)
 
         if form.is_valid():
@@ -36,6 +41,7 @@ def questionnaire(request, doc_id):
                     sec = int(match.group('sec'))
                     q = int(match.group('q'))
                     doc['systems'][sys]['sections'][sec]['questions'][q]['answer'] = form.cleaned_data[f]
+
             result = db.save_doc(doc)
             if result['ok']:
                 return HttpResponseRedirect(reverse('documents.views.validate', args=(doc_id, )))
@@ -48,6 +54,20 @@ def questionnaire(request, doc_id):
 
     doc['id'] = doc['_id']
     doc['rev'] = doc['_rev']
+    pp.pprint(form_extra_data)
+    # appending extra attributes to the form fields
+    for f in form:
+        match = pattern.match(str(f.name))
+        if match:
+            sys = int(match.group('sys'))
+            sec = int(match.group('sec'))
+            q = int(match.group('q'))
+            form_extra_data[f.name] = {
+                'system': doc['systems'][sys]['name'],
+                'section': doc['systems'][sys]['sections'][sec]['header']
+                    }
+        f.field.extra = form_extra_data[f.name]
+
     return render_to_response('questionnaire/form.html',
                           {'form': form, 'doc': doc},
                           context_instance=RequestContext(request))
